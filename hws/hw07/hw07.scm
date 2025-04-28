c7bb0148b8422a6b7e21b9466bfff853a45b5b09
PM25OO <3210636476@qq.com>
2025-04-28 23:02:02 +0800

    Finished q01

--- hws/hw07/hw07.scm
+++ hws/hw07/hw07.scm
@@ -1,6 +1,11 @@
 (define (square n) (* n n))
 
-(define (pow base exp) 'YOUR-CODE-HERE)
+(define (pow base exp)
+  (if (odd? exp)
+      (* base (pow base (- exp 1)))
+      (if (= exp 0)
+          1
+          (pow (square base) (/ exp 2)))))
 
 (define (repeatedly-cube n x)
   (if (zero? n)
