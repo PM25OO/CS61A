343ccac1be56b8c72762dc16c0074b38f960eb3f
PM25OO <3210636476@qq.com>
2025-04-29 17:49:11 +0800

    Format codes

--- hws/hw07/hw07.scm
+++ hws/hw07/hw07.scm
@@ -5,13 +5,19 @@
       (* base (pow base (- exp 1)))
       (if (= exp 0)
           1
-          (pow (square base) (/ exp 2)))))
+          (pow (square base) (/ exp 2))
+      )
+  )
+)
 
 (define (repeatedly-cube n x)
   (if (zero? n)
       x
-      (let (_________________)
-        (* y y y))))
+      (let ((y (repeatedly-cube (- n 1) x)))
+        (* y y y)
+      )
+  )
+)
 
 (define (cddr s) (cdr (cdr s)))
 
