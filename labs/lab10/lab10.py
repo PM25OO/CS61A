34b9c6778f30cca86a62526316b6db3bff7d1af6
PM25OO <3210636476@qq.com>
2025-06-04 19:47:11 +0800

    Finish Q2

--- labs/lab10/lab10.py
+++ labs/lab10/lab10.py
@@ -14,14 +14,14 @@ def calc_eval(exp):
     3
     """
     if isinstance(exp, Pair):
-        operator = ____________ # UPDATE THIS FOR Q2, e.g (+ 1 2), + is the operator
-        operands = ____________ # UPDATE THIS FOR Q2, e.g (+ 1 2), 1 and 2 are operands
+        operator = exp.first # UPDATE THIS FOR Q2, e.g (+ 1 2), + is the operator
+        operands = exp.rest # UPDATE THIS FOR Q2, e.g (+ 1 2), 1 and 2 are operands
         if operator == 'and': # and expressions
             return eval_and(operands)
         elif operator == 'define': # define expressions
             return eval_define(operands)
         else: # Call expressions
-            return calc_apply(___________, ___________) # UPDATE THIS FOR Q2, what is type(operator)?
+            return calc_apply(OPERATORS[operator], operands) # UPDATE THIS FOR Q2, what is type(operator)?
     elif exp in OPERATORS:   # Looking up procedures
         return OPERATORS[exp]
     elif isinstance(exp, int) or isinstance(exp, bool):   # Numbers and booleans
@@ -52,6 +52,10 @@ def floor_div(args):
     20
     """
     "*** YOUR CODE HERE ***"
+    result = calc_eval(args.first) // calc_eval(args.rest.first)
+    if args.rest.rest:
+        return floor_div(Pair(result, args.rest.rest))
+    return result
 
 scheme_t = True   # Scheme's #t
 scheme_f = False  # Scheme's #f
